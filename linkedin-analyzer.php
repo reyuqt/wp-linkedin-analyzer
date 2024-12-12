2022776f134b9bf8a478060b0186f202032fc57c
lauren <reyuqt01@gmail.com>
2024-12-11 19:02:06 -0500

    Initial commit dependencies and entry file

---
+++ linkedin-analyzer.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name: LinkedIn Profile PDF Analyzer
+ * Plugin URI:  https://apply4you.com
+ * Description: A plugin to upload and analyze a linkedin pdf
+ * Version:     2.2.1
+ * Author:      Lauren
+ * Author URI:  http://apply4you.com
+ * License:     GPL2
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
+require_once(plugin_dir_path(__FILE__) . 'includes/upload.php');
+require_once(plugin_dir_path(__FILE__) . 'includes/loading.php');
+require_once(plugin_dir_path(__FILE__) . 'includes/display.php');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
